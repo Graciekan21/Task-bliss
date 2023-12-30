e2d6f8c76fefe4b2304b2c67d76203602b4fbb9a
Graciekan21 <>
2023-12-30 19:29:18 +0000

    Create loop that run the app

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Method to check python
+# Loop runs the app
 task = []
 if __name__ == '__main__':
+
+# This creates an infinite loop
+print("To do list app !")
+while true:
+
+# This will print an empty line in each iteration of the loop
+print('\n')
+print('Select one of the options bellow')
+print('-----------')
+
+## Menu options 
+print('1. Add a new task')
+print('2. Delete a task')
+print('3. List tasks')
+print('4. Quit')
