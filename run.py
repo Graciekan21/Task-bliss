e4243bc4984f4eba6488a730f7a6fe155680b9d5
Graciekan21 <>
2023-12-31 12:26:05 +0000

    corrected error in the function for user to add task line 18

--- run.py
+++ run.py
@@ -1,18 +1,36 @@
-# creats Loop that runs the app
+"""
+ToDo List App
+
+This implements a command-line to-do list App
+that is functional for tasks.
+
+How to use:
+- Follow the script to start the Todo List App.
+
+Functions:
+"""
+## creats Loop that runs the app
 task = []
 
-# function user to add task
+## Function for user to add task
 def add_task(new_task):
     task = input('please enter a task: ')
-    print(f'task added: {new_task}')
+    tasks.appen(task)
+    print(f'task '{Task}' added to the list.')
 
+def main():
+	    print('To-do list')
+	    add_task('Take kids to school')
+	    add_task('Bake cakes that where ordered', )
+        
+	    
 if __name__ == '__main__':
 
-# This creates an infinite loop
-print("welcome to do list app !")
+## This creates an infinite loop
+print("welcome to do list app ")
 while true:
 
-# This will print an empty line in each iteration of the loop
+## This will print an empty line in each iteration of the loop
 print('\n')
 print('Select one of the options bellow')
 print('-----------')
