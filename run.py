958dab1a3f70e6689373e1b8b8469d7a75892269
Graciekan21 <>
2024-01-02 13:03:56 +0000

    correcting inditations so that the tree runs as it should

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-##.pppppcreats Loop that runs the app
+##.This creats Loop that runs the app
 tasks = []
 
 ## Function for user to add task
@@ -13,7 +13,7 @@ def list_tasks():
     View tasks in the list
     """
 if len(tasks) == 0:
-	print('No tasks.')
+	print('To-Do List.')
 else:
     print("List of tasks:")
 for i, task in enumerate(tasks):
@@ -21,14 +21,15 @@ for i, task in enumerate(tasks):
 
 ## Delete task from the To-do list
 
-def delete_tasks():        
-    
+def delete_tasks(): 
+    list_tasks() 
+
     if len(tasks) == 0:
         print('No tasks to delete.')
     else:
         print('Tasks')
         for i, task in enumerate(tasks):
-        print(f'{i+1}. {task}')
+            print(f'{i+1}. {task}')
         choice = int(input('Enter task number to delete:'))  
 
     if 0 < choice <= len(tasks):
@@ -45,25 +46,27 @@ def main():
 ###  This creates an infinite loop
 
 while True:
-        print('1. Add a new task')
-        print('2. List a task')
-        print('3. Delete a task')
-        print('4. Quit')
-## This calls all the function to perform as they should
-choice = int(input("Enter your choice: ")) 
+    print('1. Add a new task')
+    print('2. List a task')
+    print('3. Delete a task')
+    print('4. Quit')
+    ## This calls all the function to perform as they should
+    choice = int(input("Enter your choice: ")) 
 
-if choice == 1:
-      add_task()
-elif  choice == 2:
-      list_tasks()
+    if choice == 1:
+        add_task()
+    elif choice == 2:
+        list_tasks()
 
-elif choice == 3:
-     delete_task()
+    elif choice == 3:
+        
+        delete_task()
 
-elif choice == 4:  
-        print('Good Luck using the To-Do-List Application.') 
-        break
-else:
+    elif choice == 4: 
+        print('Good Luck using the To-Do-List Application.')
+
+
+    else:
         print('Invalid choice. Please try again. ')
 
 if __name__ == '__main__':
